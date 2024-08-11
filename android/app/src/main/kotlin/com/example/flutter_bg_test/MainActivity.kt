8bf84cb0de92bfdb9cd2beb3e45d922ff3e21459
peebaKU <baramee.254450@gmail.com>
2024-08-11 15:19:57 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_bg_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_bg_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,250 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
+
+void main() {
+  // Initialize port for communication between TaskHandler and UI.
+  FlutterForegroundTask.initCommunicationPort();
+  runApp(const ExampleApp());
+}
+
+// The callback function should always be a top-level function.
+@pragma('vm:entry-point')
+void startCallback() {
+  // The setTaskHandler function must be called to handle the task in the background.
+  FlutterForegroundTask.setTaskHandler(MyTaskHandler());
+}
+
+class MyTaskHandler extends TaskHandler {
+  int _count = 0;
+
+  // Called when the task is started.
+  @override
+  void onStart(DateTime timestamp) {
+    print('onStart');
+  }
+
+  // Called every [ForegroundTaskOptions.interval] milliseconds.
+  @override
+  void onRepeatEvent(DateTime timestamp) {
+    FlutterForegroundTask.updateService(notificationText: 'count: $_count');
+
+    // Send data to main isolate.
+    FlutterForegroundTask.sendDataToMain(_count);
+
+    _count++;
+  }
+
+  // Called when the task is destroyed.
+  @override
+  void onDestroy(DateTime timestamp) {
+    print('onDestroy');
+  }
+
+  // Called when data is sent using [FlutterForegroundTask.sendDataToTask].
+  @override
+  void onReceiveData(Object data) {
+    print('onReceiveData: $data');
+  }
+
+  // Called when the notification button is pressed.
+  @override
+  void onNotificationButtonPressed(String id) {
+    print('onNotificationButtonPressed: $id');
+  }
+
+  // Called when the notification itself is pressed.
+  //
+  // AOS: "android.permission.SYSTEM_ALERT_WINDOW" permission must be granted
+  // for this function to be called.
+  @override
+  void onNotificationPressed() {
+    FlutterForegroundTask.launchApp('/');
+    print('onNotificationPressed');
+  }
+
+  // Called when the notification itself is dismissed.
+  //
+  // AOS: only work Android 14+
+  // iOS: only work iOS 10+
+  @override
+  void onNotificationDismissed() {
+    print('onNotificationDismissed');
+  }
+}
+
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => const ExamplePage(),
+      },
+      initialRoute: '/',
+    );
+  }
+}
+
+class ExamplePage extends StatefulWidget {
+  const ExamplePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ExamplePageState();
+}
+
+class _ExamplePageState extends State<ExamplePage> {
+  Future<void> _requestPermissions() async {
+    // Android 13+, you need to allow notification permission to display foreground service notification.
+    //
+    // iOS: If you need notification, ask for permission.
+    final NotificationPermission notificationPermissionStatus =
+        await FlutterForegroundTask.checkNotificationPermission();
+    if (notificationPermissionStatus != NotificationPermission.granted) {
+      await FlutterForegroundTask.requestNotificationPermission();
+    }
+
+    if (Platform.isAndroid) {
+      // "android.permission.SYSTEM_ALERT_WINDOW" permission must be granted for
+      // onNotificationPressed function to be called.
+      //
+      // When the notification is pressed while permission is denied,
+      // the onNotificationPressed function is not called and the app opens.
+      //
+      // If you do not use the onNotificationPressed or launchApp function,
+      // you do not need to write this code.
+      if (!await FlutterForegroundTask.canDrawOverlays) {
+        // This function requires `android.permission.SYSTEM_ALERT_WINDOW` permission.
+        await FlutterForegroundTask.openSystemAlertWindowSettings();
+      }
+
+      // Android 12+, there are restrictions on starting a foreground service.
+      //
+      // To restart the service on device reboot or unexpected problem, you need to allow below permission.
+      if (!await FlutterForegroundTask.isIgnoringBatteryOptimizations) {
+        // This function requires `android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS` permission.
+        await FlutterForegroundTask.requestIgnoreBatteryOptimization();
+      }
+    }
+  }
+
+  Future<void> _initService() async {
+    FlutterForegroundTask.init(
+      androidNotificationOptions: AndroidNotificationOptions(
+        channelId: 'foreground_service',
+        channelName: 'Foreground Service Notification',
+        channelDescription:
+            'This notification appears when the foreground service is running.',
+        channelImportance: NotificationChannelImportance.LOW,
+        priority: NotificationPriority.LOW,
+      ),
+      iosNotificationOptions: const IOSNotificationOptions(
+        showNotification: true,
+        playSound: false,
+      ),
+      foregroundTaskOptions: const ForegroundTaskOptions(
+        interval: 5000,
+        isOnceEvent: false,
+        autoRunOnBoot: true,
+        autoRunOnMyPackageReplaced: true,
+        allowWakeLock: true,
+        allowWifiLock: true,
+      ),
+    );
+  }
+
+  Future<ServiceRequestResult> _startService() async {
+    if (await FlutterForegroundTask.isRunningService) {
+      return FlutterForegroundTask.restartService();
+    } else {
+      return FlutterForegroundTask.startService(
+        serviceId: 256,
+        notificationTitle: 'Foreground Service is running',
+        notificationText: 'Tap to return to the app',
+        notificationIcon: null,
+        notificationButtons: [
+          const NotificationButton(id: 'btn_hello', text: 'hello'),
+        ],
+        callback: startCallback,
+      );
+    }
+  }
+
+  Future<ServiceRequestResult> _stopService() async {
+    return FlutterForegroundTask.stopService();
+  }
+
+  void _onReceiveTaskData(dynamic data) {
+    if (data is int) {
+      print('count: $data');
+    }
+  }
+
+  void _sendRandomData() {
+    final Random random = Random();
+    final int data = random.nextInt(100);
+    FlutterForegroundTask.sendDataToTask(data);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Add a callback to receive data sent from the TaskHandler.
+    FlutterForegroundTask.addTaskDataCallback(_onReceiveTaskData);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // Request permissions and initialize the service.
+      _requestPermissions();
+      _initService();
+    });
+  }
+
+  @override
+  void dispose() {
+    // Remove a callback to receive data sent from the TaskHandler.
+    FlutterForegroundTask.removeTaskDataCallback(_onReceiveTaskData);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // ** optional **
+    // A widget that minimize the app without closing it when the user presses
+    // the soft back button. It only works when the service is running.
+    //
+    // This widget must be declared above the [Scaffold] widget.
+    return WithForegroundTask(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Foreground Task'),
+          centerTitle: true,
+        ),
+        body: _buildContentView(),
+      ),
+    );
+  }
+
+  Widget _buildContentView() {
+    buttonBuilder(String text, {VoidCallback? onPressed}) {
+      return ElevatedButton(
+        onPressed: onPressed,
+        child: Text(text),
+      );
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          buttonBuilder('start service', onPressed: _startService),
+          buttonBuilder('stop service', onPressed: _stopService),
+          buttonBuilder('send random data', onPressed: _sendRandomData),
+        ],
+      ),
+    );
+  }
+}
